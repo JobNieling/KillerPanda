3028967d3fa28790bc6503bf80bbb9f5bdb1b3f7
KimCorn <k.cornelissen3@student.han.nl>
2022-03-29 15:34:36 +0200

    main

--- src/main/java/com/github/hanyaeger/tutorial/KillerPanda.java
+++ src/main/java/com/github/hanyaeger/tutorial/KillerPanda.java
@@ -1,5 +1,23 @@
 package com.github.hanyaeger.tutorial;
 
-public class KillerPanda {
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+
+public class KillerPanda extends YaegerGame{
+		
+		public static void main(String[] args) {
+			launch(args);
+		}
+
+		@Override
+		public void setupGame() {
+			setGameTitle("Killer Panda");
+			setSize(new Size(800, 600));
+		}
+
+		@Override
+		public void setupScenes() {
+			
+		}
 
 }
